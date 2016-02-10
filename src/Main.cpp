96a8284f1c6757a0466c23c2a1eb24258ba4343b
inigo.odriozola <inigo.odriozola@PC-odriozola.avitech-ag.intra>
2016-02-10 19:21:15 +0100

    Main added

---
+++ src/Main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 10 Feb 2016
+ *      Author: inigo.odriozola
+ */
+
+#include "Main.h"
+
+Main::Main() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Main::~Main() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Main.h
@@ -0,0 +1,17 @@
+/*
+ * Main.h
+ *
+ *  Created on: 10 Feb 2016
+ *      Author: inigo.odriozola
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+class Main {
+public:
+	Main();
+	virtual ~Main();
+};
+
+#endif /* MAIN_H_ */
